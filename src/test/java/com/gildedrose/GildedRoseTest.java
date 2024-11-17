d6021299003469f3d7b8bbdb334d5423b289e499
wallace.drew <wallace.drew@gmail.com>
2024-11-17 16:37:38 -0500

    change signature

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,14 +8,18 @@ class GildedRoseTest {
 
     @Test
     void updateQuality_returnsNameOfFirstItem() {
-        GildedRose app = createGildedRose("foo", 0, 0);
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
+        GildedRose app1 = new GildedRose(items);
+        GildedRose app = app1;
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
     }
 
     @Test
     void updateQuality_updateAgedBrie() {
-        GildedRose app = createGildedRose("Aged Brie", 2, 0);
+        Item[] items = new Item[]{new Item("Aged Brie", 2, 0)};
+        GildedRose app1 = new GildedRose(items);
+        GildedRose app = app1;
         app.updateQuality();
         assertEquals(1, app.items[0].sellIn);
         assertEquals(1, app.items[0].quality);
@@ -25,14 +29,14 @@ class GildedRoseTest {
     void updateQuality_updateDexterity() {
         Item expectedItem = new Item("+5 Dexterity Vest", 9, 19);
 
-        GildedRose app = createGildedRose("+5 Dexterity Vest", 10, 20);
+        GildedRose app = createGildedRose(new Item("+5 Dexterity Vest", 10, 20));
         app.updateQuality();
 
         assertEquals(expectedItem.toString(), app.items[0].toString());
     }
 
-    private static GildedRose createGildedRose(String foo, int sellIn, int quality) {
-        Item[] items = new Item[]{new Item(foo, sellIn, quality)};
+    private static GildedRose createGildedRose(Item item) {
+        Item[] items = new Item[]{new Item(item.name, item.sellIn, item.quality)};
         GildedRose app = new GildedRose(items);
         return app;
     }
