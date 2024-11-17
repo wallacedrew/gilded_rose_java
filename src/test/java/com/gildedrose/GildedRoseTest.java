15953f884df1cc9756b4bcc2386f40c4fe817db5
wallace.drew <wallace.drew@gmail.com>
2024-11-17 17:08:36 -0500

    remove duplication of asserts

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -10,65 +10,47 @@ class GildedRoseTest {
     void updateQuality_updateAgedBrie() {
         Item originalItem = new Item("Aged Brie", 2, 0);
         Item expectedItem = expectedUpdate(originalItem.name, 1, 1);
-        GildedRose app = setupGildedRose(originalItem);
-
-        app.updateQuality();
-
-        assertEquals(expectedItem.toString(), app.items[0].toString());
+        validate(originalItem, expectedItem);
     }
 
     @Test
     void updateQuality_updateDexterity() {
         Item originalItem = new Item("+5 Dexterity Vest", 10, 20);
         Item expectedItem = expectedUpdate(originalItem.name, 9, 19);
-        GildedRose app = setupGildedRose(originalItem);
-
-        app.updateQuality();
-
-        assertEquals(expectedItem.toString(), app.items[0].toString());
+        validate(originalItem, expectedItem);
     }
 
     @Test
     void updateQuality_updateElixir() {
         Item originalItem = new Item("Elixir of the Mongoose", 5, 7);
         Item expectedItem = expectedUpdate(originalItem.name, 4, 6);
-        GildedRose app = setupGildedRose(originalItem);
-
-        app.updateQuality();
-
-        assertEquals(expectedItem.toString(), app.items[0].toString());
+        validate(originalItem, expectedItem);
     }
 
     @Test
     void updateQuality_updateSulfuras() {
         Item originalItem = new Item("Sulfuras, Hand of Ragnaros", -1, 80);
         Item expectedItem = expectedUpdate(originalItem.name, -1, 80);
-        GildedRose app = setupGildedRose(originalItem);
-
-        app.updateQuality();
-
-        assertEquals(expectedItem.toString(), app.items[0].toString());
+        validate(originalItem, expectedItem);
     }
 
     @Test
     void updateQuality_updateBackstage() {
         Item originalItem = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49);
         Item expectedItem = expectedUpdate(originalItem.name, 4, 50);
-        GildedRose app = setupGildedRose(originalItem);
-
-        app.updateQuality();
-
-        assertEquals(expectedItem.toString(), app.items[0].toString());
+        validate(originalItem, expectedItem);
     }
 
     @Test
     void updateQuality_updateConjured() {
         Item originalItem = new Item("Conjured Mana Cake", 3, 6);
         Item expectedItem = expectedUpdate(originalItem.name, 2, 5);
+        validate(originalItem, expectedItem);
+    }
+
+    private static void validate(Item originalItem, Item expectedItem) {
         GildedRose app = setupGildedRose(originalItem);
-
         app.updateQuality();
-
         assertEquals(expectedItem.toString(), app.items[0].toString());
     }
 
@@ -78,8 +60,7 @@ class GildedRoseTest {
 
     private static GildedRose setupGildedRose(Item item) {
         Item[] items = new Item[]{new Item(item.name, item.sellIn, item.quality)};
-        GildedRose app = new GildedRose(items);
-        return app;
+        return new GildedRose(items);
     }
 
 }
