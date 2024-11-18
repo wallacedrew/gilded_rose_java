6bf98d4a7e7a56d48c1a93dcf20bcb1597bf51e7
wallace.drew <wallace.drew@gmail.com>
2024-11-17 21:50:54 -0500

    remove duplication

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -37,45 +37,33 @@ class GildedRose {
             }
 
             if (item.name.equals(ItemName.DEXTERITY)) {
-                if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
+                updateItems(item);
             }
 
             if (item.name.equals(ItemName.ELIXIR)) {
-                if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
+                updateItems(item);
             }
 
             if (item.name.equals(ItemName.CONJURED)) {
-                if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
+                updateItems(item);
             }
 
             if (!item.name.equals(ItemName.SULFURAS)) {
                 item.sellIn = item.sellIn - 1;
             }
 
+        }
+    }
+
+    private static void updateItems(Item item) {
+        if (item.sellIn < 0) {
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
+            }
+        }
+
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
         }
     }
 }
