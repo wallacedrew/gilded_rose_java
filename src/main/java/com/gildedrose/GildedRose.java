3453a83a6f9a5dfee4180d4590c2dab784531263
wallace.drew <wallace.drew@gmail.com>
2024-11-17 20:50:08 -0500

    slide statements for cohesive reading order

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -12,16 +12,6 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
             Item item = items[i];
 
-            if (!item.name.equals(ItemName.AGED_BRIE)) {
-                if (!item.name.equals(ItemName.BACKSTAGE)) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals(ItemName.SULFURAS)) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                }
-            }
-
             if (item.name.equals(ItemName.AGED_BRIE)) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
@@ -47,8 +37,14 @@ class GildedRose {
                 }
             }
 
-            if (!item.name.equals(ItemName.SULFURAS)) {
-                item.sellIn = item.sellIn - 1;
+            if (!item.name.equals(ItemName.AGED_BRIE)) {
+                if (!item.name.equals(ItemName.BACKSTAGE)) {
+                    if (item.quality > 0) {
+                        if (!item.name.equals(ItemName.SULFURAS)) {
+                            item.quality = item.quality - 1;
+                        }
+                    }
+                }
             }
 
             if (item.sellIn < 0) {
@@ -71,6 +67,11 @@ class GildedRose {
                     }
                 }
             }
+
+            if (!item.name.equals(ItemName.SULFURAS)) {
+                item.sellIn = item.sellIn - 1;
+            }
+
         }
     }
 }
