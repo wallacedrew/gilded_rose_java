be502d076848cbc5f0986b334b5b5ef4139fcf4e
wallace.drew <wallace.drew@gmail.com>
2024-11-18 03:06:22 -0500

    move factory method to Updater

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -13,32 +13,8 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             Item item = items[i];
-            Updater updater = createUpdater(item);
+            Updater updater = Updater.createUpdater(item);
             updater.update(item);
         }
     }
-
-    private static Updater createUpdater(Item item) {
-        Updater updater = null;
-        if (item.name.equals(ItemName.BACKSTAGE)) {
-            updater = new BackstageUpdater();
-        }
-        else if (item.name.equals(ItemName.AGED_BRIE)) {
-            updater = new AgedBrieUpdater();
-        }
-        else if (item.name.equals(ItemName.DEXTERITY)) {
-            updater = new DexterityUpdater();
-        }
-        else if (item.name.equals(ItemName.ELIXIR)) {
-            updater = new ElixirUpdater();
-        }
-        else if (item.name.equals(ItemName.CONJURED)) {
-            updater = new ConjureUpdater();
-        }
-        else if (item.name.equals(ItemName.SULFURAS)) {
-            updater = new SulfurasUpdater();
-        }
-        return updater;
-    }
-
 }
