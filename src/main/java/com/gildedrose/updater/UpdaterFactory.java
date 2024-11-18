74599818d88bc81a72f473a81c5317f2d3b9ab27
wallace.drew <wallace.drew@gmail.com>
2024-11-18 03:26:26 -0500

    simplify creation method

--- src/main/java/com/gildedrose/updater/UpdaterFactory.java
+++ src/main/java/com/gildedrose/updater/UpdaterFactory.java
@@ -6,26 +6,27 @@ import com.gildedrose.ItemName;
 public abstract class UpdaterFactory {
 
     public static Updater createUpdater(Item item) {
-        Updater updater = null;
         if (item.name.equals(ItemName.BACKSTAGE)) {
-            updater = new BackstageUpdater(item);
+            return new BackstageUpdater(item);
         }
         else if (item.name.equals(ItemName.AGED_BRIE)) {
-            updater = new AgedBrieUpdater(item);
+            return new AgedBrieUpdater(item);
         }
         else if (item.name.equals(ItemName.DEXTERITY)) {
-            updater = new DexterityUpdater(item);
+            return new DexterityUpdater(item);
         }
         else if (item.name.equals(ItemName.ELIXIR)) {
-            updater = new ElixirUpdater(item);
+            return new ElixirUpdater(item);
         }
         else if (item.name.equals(ItemName.CONJURED)) {
-            updater = new ConjureUpdater(item);
+            return new ConjureUpdater(item);
         }
         else if (item.name.equals(ItemName.SULFURAS)) {
-            updater = new SulfurasUpdater(item);
+            return new SulfurasUpdater(item);
         }
-        return updater;
+        else {
+            throw new RuntimeException("unknown item name");
+        }
     }
 
 }
